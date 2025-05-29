30b32d47642301802a08774a95e7a14a5dcbaef2
CamiloM21 <164238690+CamiloM21@users.noreply.github.com>
2025-05-29 06:11:51 +0000

    .

--- app.py
+++ app.py
@@ -5,42 +5,44 @@ import numpy as np
 
 app = FastAPI()
 
-# Cargar modelo y transformadores
-model = joblib.load("random_forest_model.bin")     # Modelo entrenado
-scaler = joblib.load("scaler_pca.bin")             # Escalador
-pca = joblib.load("pca.bin")                       # Reducción PCA
+model = joblib.load("random_forest_model.bin")
+scaler = joblib.load("scaler_pca.bin")
+pca = joblib.load("pca.bin")
 
 class AirQualityInput(BaseModel):
     PM25: float
     PM10: float
     NO2: float
-    O3: float
     SO2: float
     CO: float
+    O3: float
+    Temperature: float
+    Humidity: int
+    WindSpeed: float
 
 @app.post("/predict")
 def predict(input_data: AirQualityInput):
     try:
         print("Datos recibidos:", input_data)
-        # Crear el vector de entrada
+
         row = np.array([
             input_data.PM25,
             input_data.PM10,
             input_data.NO2,
-            input_data.O3,
             input_data.SO2,
-            input_data.CO
+            input_data.CO,
+            input_data.O3,
+            input_data.Temperature,
+            input_data.Humidity,
+            input_data.WindSpeed
         ]).reshape(1, -1)
 
-        # Escalar
         X_scaled = scaler.transform(row)
         print("Datos escalados:", X_scaled)
 
-        # Reducir dimensiones con PCA
         X_pca = pca.transform(X_scaled)
         print("Datos después de PCA:", X_pca)
 
-        # Predecir
         prediction = model.predict(X_pca)[0]
         print("Predicción:", prediction)
 
